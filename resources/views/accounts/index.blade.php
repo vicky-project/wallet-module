d4c22b8a6862204718b3f7b8f36130c3754543cf
vicky <pratamavr@gmail.com>
2025-12-23 10:00:08 +0800

    updates

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="d-flex justify-content-between align-items-center mb-4">
   <h1><i class="fas fa-wallet"></i> My Accounts</h1>
-  <a href="{{ route('apps.accounts.create') }}" class="btn bg-primary">
+  <a href="{{ route('apps.wallet.create') }}" class="btn bg-primary">
     <i class="fas fa-fw fa-plus"></i>
   </a>
 </div>
@@ -17,7 +17,7 @@
 <div class="row">
   @forelse ($accounts as $account)
   <div class="col col-md-6 col-lg-4 mb-4">
-    <div class="card wallet-card h-100" onclick="window.location='{{ route('apps.accounts.show', $account) }}'">
+    <div class="card wallet-card h-100" onclick="window.location='{{ route('apps.wallet.show', $account) }}'">
       <div class="card-body">
         <div class="d-flex justify-content-between align-items-start">
           <div></div>
