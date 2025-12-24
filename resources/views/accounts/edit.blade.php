6d5b4d8f0283359a6cada2d5cf65ec438b452474
vicky <pratamavr@gmail.com>
2025-12-24 10:12:09 +0800

    updates

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -8,7 +8,7 @@
 <div class="card">
   <div class="card-header text-end">
     <div class="float-start me-auto">
-      <a href="{{ route('apps.wallet.index') }}" class="btn btn-secondary" role="button">
+      <a href="{{ route('apps.wallet.show', $account) }}" class="btn btn-secondary" role="button">
         <i class="fas fa-arrow-left"></i>
       </a>
     </div>
