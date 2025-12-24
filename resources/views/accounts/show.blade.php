9609223dd55f5f6ea5b39d80e050046355811202
vicky <pratamavr@gmail.com>
2025-12-24 18:53:47 +0800

    updates

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -26,6 +26,7 @@
       <div class="card-body">
         <h5 class="card-title">{{ $wallet->name }}</h5>
         <h3 class="text-success">
+          {{ dd(number_format((float) $wallet->balance, 2, ',', '.')) }}
           <x-money amount="{{number_format((float) $wallet->balance, 2, '.', ',')}}" currency="{{ $wallet->meta['currency']}}" />
         </h3>
         <p class="card-text">
