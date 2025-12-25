002d14c103ce2166f81f8fe2612df1c9d382ef83
vicky <pratamavr@gmail.com>
2025-12-25 18:10:21 +0800

    updates

--- app/Enums/AccountType.php
+++ app/Enums/AccountType.php
@@ -4,5 +4,8 @@ namespace Modules\Wallet\Enums;
 enum AccountType: string
 {
 	case GENERAL = "general";
-	case PRIVATE = "private";
+	case CASH = "cash";
+	case SAVING = "saving";
+	case CHECKING = "checking";
+	case INVESTMENT = "investment";
 }
