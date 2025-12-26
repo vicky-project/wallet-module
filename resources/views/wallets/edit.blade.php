00584dd783cfbe260e4acda7f64b1e9a0e755080
vicky <pratamavr@gmail.com>
2025-12-26 10:23:49 +0800

    updates

--- resources/views/wallets/edit.blade.php
+++ resources/views/wallets/edit.blade.php
@@ -18,8 +18,8 @@
     <form method="POST" action="{{ route('apps.wallets.update', $wallet) }}" class="needs-validation" novalidate>
       @csrf
       @method('PUT')
-      <div class="row mb-3">
-        <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-6 mb-3">
           <label for="wallet-account_id" class="form-label">Account</label>
           <select name="account_id" class="form-select" id="wallet-account_id">
             @foreach($accounts as $account)
@@ -27,13 +27,13 @@
             @endforeach
           </select>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 mb-3">
           <label for="wallet-name" class="form-label">Wallet Name <span class="text-danger">*</span></label>
           <input type="text" class="form-control" name="name" id="wallet-name" value="{{ old('name', $wallet->name) }}" placeholder="Enter wallet name..." required>
         </div>
       </div>
-      <div class="row mb-3">
-        <div class="col-md-4">
+      <div class="row">
+        <div class="col-md-4 mb-3">
           <label for="wallet-type" class="form-label">Type</label>
           <select name="type" class="form-select" id="wallet-type">
             @foreach(WalletType::cases() as $type)
@@ -41,11 +41,11 @@
             @endforeach
           </select>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
           <label for="wallet-initial-balance" class="form-label">Initial Balance</label>
           <input type="number" min="0" name="initial_balance" class="form-control" id="wallet-initial-balance" value="{{ old('initial_balance', $wallet->initial_balance) }}">
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
           <label for="account-currency" class="form-label">Currency</label>
           <select class="form-select" name="currency" id="account-currency">
             @foreach($currencies as $currency => $name)
@@ -54,8 +54,8 @@
           </select>
         </div>
       </div>
-      <div class="row mb-3">
-        <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-12 mb-3">
           <label for="wallet-description" class="form-label">Description</label>
           <textarea name="description" id="wallet-description" class="form-control" placeholder="Description of wallet..">{{ old('description', $wallet->description) }}</textarea>
         </div>
