c061b43c37668554c18174bfdfc57a97bb383ecb
vicky <pratamavr@gmail.com>
2025-12-26 10:19:20 +0800

    updates

--- resources/views/wallets/edit.blade.php
+++ resources/views/wallets/edit.blade.php
@@ -18,7 +18,7 @@
     <form method="POST" action="{{ route('apps.wallets.update', $wallet) }}" class="needs-validation" novalidate>
       @csrf
       @method('PUT')
-      <div class="row">
+      <div class="row mb-3">
         <div class="col-md-6">
           <label for="wallet-account_id" class="form-label">Account</label>
           <select name="account_id" class="form-select" id="wallet-account_id">
@@ -31,6 +31,8 @@
           <label for="wallet-name" class="form-label">Wallet Name <span class="text-danger">*</span></label>
           <input type="text" class="form-control" name="name" id="wallet-name" value="{{ old('name', $wallet->name) }}" placeholder="Enter wallet name..." required>
         </div>
+      </div>
+      <div class="row mb-3">
         <div class="col-md-4">
           <label for="wallet-type" class="form-label">Type</label>
           <select name="type" class="form-select" id="wallet-type">
@@ -41,7 +43,7 @@
         </div>
         <div class="col-md-4">
           <label for="wallet-initial-balance" class="form-label">Initial Balance</label>
-          <input type="number" min="0" name="initial_balance" class="form-control" id="wallet-initial-balance" value="{{ old('initial_balance', $wallet->initial_balance ?? 0) }}">
+          <input type="number" min="0" name="initial_balance" class="form-control" id="wallet-initial-balance" value="{{ old('initial_balance', $wallet->initial_balance 0) }}">
         </div>
         <div class="col-md-4">
           <label for="account-currency" class="form-label">Currency</label>
