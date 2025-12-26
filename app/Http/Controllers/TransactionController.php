efe1e0793e50315b4d3190fb6f5c01871a288444
vicky <pratamavr@gmail.com>
2025-12-26 20:41:52 +0800

    updates

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -54,7 +54,7 @@ class TransactionController extends BaseController
 			$request->all()
 		);
 		dd(
-			$transactions,
+			$transactions->map(fn($item) => dd($item)),
 			$transactions->map(
 				fn($item) => [
 					"total" => $item->count(),
