37871d1c5203bb50c72692e3b98ec27d22f2150a
vicky <pratamavr@gmail.com>
2025-12-26 10:20:18 +0800

    updates

--- resources/views/wallets/edit.blade.php
+++ resources/views/wallets/edit.blade.php
@@ -43,7 +43,7 @@
         </div>
         <div class="col-md-4">
           <label for="wallet-initial-balance" class="form-label">Initial Balance</label>
-          <input type="number" min="0" name="initial_balance" class="form-control" id="wallet-initial-balance" value="{{ old('initial_balance', $wallet->initial_balance 0) }}">
+          <input type="number" min="0" name="initial_balance" class="form-control" id="wallet-initial-balance" value="{{ old('initial_balance', $wallet->initial_balance) }}">
         </div>
         <div class="col-md-4">
           <label for="account-currency" class="form-label">Currency</label>
