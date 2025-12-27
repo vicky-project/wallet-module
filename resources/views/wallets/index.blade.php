1392b14637f3f80accda4cec3eb3da016dee1b64
vicky <pratamavr@gmail.com>
2025-12-27 11:50:45 +0800

    updates

--- resources/views/wallets/index.blade.php
+++ resources/views/wallets/index.blade.php
@@ -196,7 +196,7 @@
 </div>
 
 <div class="modal fade" id="walletTransferModal" tabindex="-1">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-fullscreen">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">Wallet Transfer</h5>
