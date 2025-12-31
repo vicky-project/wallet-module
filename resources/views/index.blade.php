9cd1c1405a6a6eea43c681725f1cb02b68328f41
vicky <pratamavr@gmail.com>
2025-12-31 12:01:51 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,7 +74,7 @@
 </div>
 <div class="row mb-4">
   <!-- Recent Transactions -->
-  <div class="col-lg-4 mb-4">
+  <div class="col-lg-8 mb-4">
     <div class="card h-100">
       <div class="card-header d-flex justify-content-between align-items-center">
         <h5 class="card-title mb-0">Transaksi Terbaru</h5>
@@ -148,7 +148,7 @@
       </div>
     </div>
   </div>
-                
+
   <div class="col-lg-4 mb-4">
     <div class="card h-100">
       <div class="card-header d-flex justify-content-between align-items-center">
