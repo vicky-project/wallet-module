eb09587a5454b600eccae8e5d8468bdf6d6a123a
vicky <pratamavr@gmail.com>
2026-01-01 15:29:32 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Wallet\Http\Controllers\WalletController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('wallets', WalletController::class)->names('wallet');
-});
+Route::middleware(["auth:sanctum"])
+	->prefix("v1")
+	->group(function () {});
