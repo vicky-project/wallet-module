7fd53045fcf4d098a07ca58b047d98f474af89b2
vicky <pratamavr@gmail.com>
2026-01-05 00:54:55 +0800

    updates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -65,7 +65,9 @@ class DashboardController extends BaseController
 		$savingGoals = $this->savingGoalRepository->getActiveGoals($user);
 
 		// Get accounts summary
-		$accounts = $this->accountRepository->getAccounts($user);
+		$accounts = $this->accountsRepository->getAccountMapping(
+			$this->accountRepository->accounts(auth()->user())
+		);
 
 		// Get categories for quick add forms
 		$incomeCategories = $this->categoryRepository->getByType("income", $user);
