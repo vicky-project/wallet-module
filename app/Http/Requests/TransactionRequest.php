4ea85fae3ed18f7ec3a202f2f5645257fd6d83a6
vicky <pratamavr@gmail.com>
2026-01-05 11:10:28 +0800

    updates

--- app/Http/Requests/TransactionRequest.php
+++ app/Http/Requests/TransactionRequest.php
@@ -34,7 +34,7 @@ class TransactionRequest extends FormRequest
 			"reference_number" => "nullable|string|max:100",
 			"description" => "nullable|string|max:5000",
 			"is_recurring" => "nullable",
-			"recurring_period" => "required_if:is_recurring|string",
+			"recurring_period" => "required_if:is_recurring,true|string",
 			"recurring_end_date" => "nullable|date",
 		];
 
