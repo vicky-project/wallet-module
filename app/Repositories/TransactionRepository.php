b0fc0cb5d5ce3445fe4eeec2cfa181201625f8f0
vicky <pratamavr@gmail.com>
2026-01-05 20:09:23 +0800

    updates

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -173,7 +173,7 @@ class TransactionRepository extends BaseRepository
 			->get()
 			->map(function ($transaction) {
 				$transaction->formatted_amount = $this->formatMoney(
-					$this->fromDatabaseAmount($transaction->amount)
+					$this->fromDatabaseAmount($transaction->amount->getAmount()->toInt())
 				);
 				return $transaction;
 			});
