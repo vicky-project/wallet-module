6338d557ffa819429161741f7a40e2ca6aea6579
vicky <pratamavr@gmail.com>
2026-01-05 15:36:51 +0800

    updates

--- resources/views/transactions/edit.blade.php
+++ resources/views/transactions/edit.blade.php
@@ -507,7 +507,9 @@
             weekday: 'long',
             year: 'numeric',
             month: 'long',
-            day: 'numeric'
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
         });
         
         const previewHTML = `
