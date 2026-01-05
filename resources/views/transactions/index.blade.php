d16350a0a62a617975d2d5e60e83b71c4313609d
vicky <pratamavr@gmail.com>
2026-01-05 15:17:06 +0800

    updates

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -11,7 +11,7 @@
     <h1 class="page-title">Daftar Transaksi</h1>
   </div>
   <div>
-    {{ $filters['month']->format('F') }} ● {{ $filters['year'] }}
+    {{ $filters['month'] }} ● {{ $filters['year'] }}
   </div>
 </div>
 
@@ -177,7 +177,7 @@
 
 <div class="card">
   <div class="card-header text-end">
-    <h5 class="card-title">{{ now($filters['month'])->format('F') }} {{ $filters['year'] }}</h5>
+    <h5 class="card-title">{{ $filters['month']) }} {{ $filters['year'] }}</h5>
   </div>
   <div class="card-body p-0">
     @if($transactions->count() > 0)
