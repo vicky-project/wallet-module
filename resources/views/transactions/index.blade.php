8f7d94b547d2af92f4291baa87a58f856d4b35c0
vicky <pratamavr@gmail.com>
2026-01-05 14:58:24 +0800

    updates

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -11,7 +11,7 @@
     <h1 class="page-title">Daftar Transaksi</h1>
   </div>
   <div>
-    {{ now(request()->has('month') ? request()->input('month') : $filters['month'])->format('F') }} ● {{ request()->has('year') ? request()->input('year') : $filters['year'] }}
+    {{ now(request()->query('month') ?? $filters['month'])->format('F') }} ● {{ request()->has('year') ? request()->input('year') : $filters['year'] }}
   </div>
 </div>
 
