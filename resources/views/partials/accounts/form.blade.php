102b80be1676f4c5d63c3d0ae8be881cefb48cc3
vicky <pratamavr@gmail.com>
2026-01-09 00:40:43 +0800

    updates

--- resources/views/partials/accounts/form.blade.php
+++ resources/views/partials/accounts/form.blade.php
@@ -209,8 +209,8 @@
       toggleBankInfo();
       
       if(typeSelect.value) {
-        const typeName = typeSelect.options[typeSelect.value];
         console.log(typeSelect.value);
+        const typeName = typeSelect.options[typeSelect.value];
         const item = accountTypeMap[typeName];
         iconInput.value = item.icon;
         iconPreview.className = item.icon;
