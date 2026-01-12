2098ad8da8545ebf114e88c5ea6534acd7aff619
vicky <pratamavr@gmail.com>
2026-01-12 11:07:08 +0800

    updates

--- resources/views/budgets/show.blade.php
+++ resources/views/budgets/show.blade.php
@@ -46,7 +46,7 @@
           </div>
         </div>
         <div class="d-flex justify-content-between text-white-75">
-          <small>{{ Number::currency($budget->spent->getAmount()->toInt(), in: 'id-ID') }} terpakai</small>
+          <small>@money($budget->spent->getAmount()->toInt()) terpakai</small>
           <small>{{ Helper::formatMoney($budget->amount->getAmount()->toInt()) }} total budget</small>
         </div>
       </div>
