9c2d6c7c1ce635813ade5dabeba095587ba8a668
vicky <pratamavr@gmail.com>
2026-01-14 16:14:43 +0800

    updates

--- app/Repositories/AccountRepository.php
+++ app/Repositories/AccountRepository.php
@@ -187,7 +187,6 @@ class AccountRepository extends BaseRepository
 		$accountCount = $accounts->count();
 
 		foreach ($accounts as $account) {
-			dd($account);
 			$balance = $account->balance->getAmount()->toInt();
 
 			if ($account->isLiability()) {
