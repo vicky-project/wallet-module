f0272a7fa84cbf8637a4ac63b6d84e4537745aed
vicky <pratamavr@gmail.com>
2026-01-15 23:20:51 +0800

    updates

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -315,7 +315,7 @@ class TransactionController extends Controller
 					break;
 			}
 
-			return response()->download($file->getFile(), $filename);
+			return response($file);
 		} catch (\Exception $e) {
 			logger()->error("Failef to export file.", [
 				"message" => $e->getMessage(),
