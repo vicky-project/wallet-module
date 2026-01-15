aac6a3060fc71428ca4c876a7218a5abc538f27e
vicky <pratamavr@gmail.com>
2026-01-15 22:54:09 +0800

    updates

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -290,7 +290,7 @@ class TransactionController extends Controller
 
 			$tmp_dir = config("excel.temporary_files.local_path");
 			if (!file_exists($tmp_dir) || !is_readable($tmp_dir)) {
-				mkdir($tmp_dir, 0775);
+				@mkdir($tmp_dir, 0775);
 			}
 
 			$filename = "transactions_" . date("Ymd_His");
