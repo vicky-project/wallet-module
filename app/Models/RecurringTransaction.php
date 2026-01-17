51b6907172c9b58a3791b1e41889623f6eb6d810
vicky <pratamavr@gmail.com>
2026-01-17 10:18:31 +0800

    updates

--- app/Models/RecurringTransaction.php
+++ app/Models/RecurringTransaction.php
@@ -1,6 +1,7 @@
 <?php
 namespace Modules\Wallet\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
