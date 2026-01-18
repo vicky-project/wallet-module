18e9c359df3406b86b3db2790af04c2a03a3391c
vicky <pratamavr@gmail.com>
2026-01-19 03:05:30 +0800

    updates

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -580,7 +580,7 @@
                 <td>
                   @if($hasBudget)
                     <div class="budget-display">
-                      {{ $category->budgets?->first()?->spent->getAmount()->toInt() }} / {{ $category->budgets->first()->amount->getAmount()->toInt() }}
+                      {{ $category->budgets?->first()?->spent->getAmount()->toInt() }} / {{ $category->budgets?->first()?->amount->getAmount()->toInt() }}
                     </div>
                     <div class="d-flex align-items-center">
                       <div class="progress progress-thin flex-grow-1 me-2">
