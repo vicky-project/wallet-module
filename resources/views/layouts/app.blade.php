ff57a80de88879559a9a9cf378bb081e0c4073aa
vicky <pratamavr@gmail.com>
2026-01-26 02:10:19 +0800

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- di dalam head tag -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>
       @yield('title', config('app.name', 'Vicky Server'))
     </title>
