2b25ae343d077de9c28ef26f401153bb381e882f
vicky <pratamavr@gmail.com>
2026-01-30 15:23:39 +0800

    updates

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<form method="POST" action="{{ route('api.apps.reports.export') }}">
+  @csrf
+  <input type="hidden" name="account_id" value="1">
+  <input type="text" name="start_date" value="{{ now()->startOfMonth() }}">
+  <input type="text" name="end_date" value="{{ now()->endOfMonth() }}">
+  <input type="text" name="format" value="pdf">
+  
+  <button type="submit">Export</button>
+</form>
