ff46320c73c7c5b9bea461afd1692f846d280aee
vicky <pratamavr@gmail.com>
2026-01-30 15:32:11 +0800

    updates

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,8 +8,8 @@
 <form method="POST" action="{{ route('api.apps.reports.export') }}">
   @csrf
   <input type="hidden" name="account_id" value="1">
-  <input type="text" name="start_date" value="{{ now()->startOfMonth() }}">
-  <input type="text" name="end_date" value="{{ now()->endOfMonth() }}">
+  <input type="date" name="start_date" value="{{ now()->startOfMonth() }}">
+  <input type="date" name="end_date" value="{{ now()->endOfMonth() }}">
   <input type="text" name="format" value="pdf">
   
   <button type="submit">Export</button>
