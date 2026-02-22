e1257d6974c4748f205693f07a26d85980170db8
vicky <pratamavr@gmail.com>
2026-02-22 20:25:29 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <div class="col-12">
         <!-- Ringkasan Saldo (di tengah) -->
         <div class="text-center my-4">
-            <small class="text-muted text-uppercase" style="letter-spacing: 1px; color: var(--tg-theme-hint-color);">Total Saldo</small>
+            <small class="text-uppercase" style="letter-spacing: 1px; color: var(--tg-theme-hint-color);">Total Saldo</small>
             <h1 class="display-4 fw-bold" style="color: var(--tg-theme-text-color);">Rp {{ number_format($totalBalance, 0, ',', '.') }}</h1>
         </div>
 
